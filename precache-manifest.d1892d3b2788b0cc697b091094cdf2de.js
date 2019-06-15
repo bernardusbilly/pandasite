7013bc036bf625a5467430d0963e50575cabd730
Billy Tjiptoning <8053504+bernardusbilly@users.noreply.github.com>
2019-06-16 01:36:01 +0700

    Deploy production build

---
+++ precache-manifest.d1892d3b2788b0cc697b091094cdf2de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51908b6310526ecc8605b0411b554047",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9df4dddec2de33e5ed5c",
+    "url": "/static/css/2.128fc69f.chunk.css"
+  },
+  {
+    "revision": "f4628c980a299ad782d2",
+    "url": "/static/css/main.f8be681a.chunk.css"
+  },
+  {
+    "revision": "9df4dddec2de33e5ed5c",
+    "url": "/static/js/2.5f920bfb.chunk.js"
+  },
+  {
+    "revision": "f4628c980a299ad782d2",
+    "url": "/static/js/main.e63ef1f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
