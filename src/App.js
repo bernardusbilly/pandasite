ed808539212a1ac98f0a5d66ca3d1a9bef896b1b
Billy Tjiptoning <8053504+bernardusbilly@users.noreply.github.com>
2019-07-03 04:33:18 +0700

    Add about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home';
+import About from './pages/about';
 
 import './App.scss';
 
@@ -11,7 +12,7 @@ class App extends Component {
       <HashRouter>
         <Switch>
           <Route exact path='/' component={Home}/>
-          <Route exact path='/about' component={Home}/>
+          <Route exact path='/about' component={About}/>
           <Route exact path='/services' component={Home}/>
           <Route exact path='/contact' component={Home}/>
         </Switch>
