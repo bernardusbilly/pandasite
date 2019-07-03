2bc878ad20183b61a1e95993ddb143da08e2e1c3
Billy Tjiptoning <8053504+bernardusbilly@users.noreply.github.com>
2019-07-03 09:18:51 +0700

    Add production build

---
+++ precache-manifest.6f080884d0c073593d94258e5b6323fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcc2c20ca52e4736a4df259cf6270c3",
+    "url": "/pandasite/build/index.html"
+  },
+  {
+    "revision": "b7a780f9614fd843696d",
+    "url": "/pandasite/build/static/css/2.128fc69f.chunk.css"
+  },
+  {
+    "revision": "8a376cc0608550f51290",
+    "url": "/pandasite/build/static/css/main.74113c1f.chunk.css"
+  },
+  {
+    "revision": "b7a780f9614fd843696d",
+    "url": "/pandasite/build/static/js/2.2c4c2ddc.chunk.js"
+  },
+  {
+    "revision": "8a376cc0608550f51290",
+    "url": "/pandasite/build/static/js/main.881d3015.chunk.js"
+  },
+  {
+    "revision": "a9a4bddf3a7934944996",
+    "url": "/pandasite/build/static/js/runtime~main.335e42e0.js"
+  }
+]);
