56e46c8a29c8328fc768dc3c2a7826ba0e1b88ea
Billy Tjiptoning <8053504+bernardusbilly@users.noreply.github.com>
2019-07-07 14:47:34 +0700

    Change to single page website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+// import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home';
-import About from './pages/about';
+// import About from './pages/about';
 
 import './App.scss';
 
 class App extends Component {
-  render() {
+  render() { 
     return (
-      <HashRouter>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/about' component={About}/>
-          <Route exact path='/services' component={Home}/>
-          <Route exact path='/contact' component={Home}/>
-        </Switch>
-      </HashRouter>
+      <Home/>
     );
   }
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -26,19 +26,19 @@ class Home extends Component {
         <div className='pd-homepage-main'>
           <div className='pd-sidebar'>
             <div className='pd-sidebar-sticky'>
-              <Link to='/' className='home-link'><Icon component={PandaSvg} style={{ fontSize: '40px' }}/></Link>
+              <a href='#home' className='home-link'><Icon component={PandaSvg} style={{ fontSize: '40px' }}/></a>
               <a href='https://www.instagram.com/pandatech.io/' className='socmed' target='_blank' rel='noopener noreferrer'><Icon type='instagram' style={{ fontSize: '40px' }} /></a>
               <a href='https://www.facebook.com/pandatech.io' className='socmed' target='_blank' rel='noopener noreferrer'><Icon type='facebook' style={{ fontSize: '40px' }}/></a>
               <div className='rotated-name'>
-                <Link to='/'>Pandatech</Link>
+                <a to='#home'>Pandatech</a>
               </div>
             </div>
           </div>
           <div className='pd-content'>
             <header>
-              <Link to='/about'>About</Link>
-              <Link to='/services'>Services</Link>
-              <Link to='/contact'>Contact</Link>
+              <a href='#about'>About</a>
+              <a href='#services'>Services</a>
+              <a href='#contact'>Contact</a>
               <Button type='link' icon='menu'/>
             </header>
             <Tabs tabPosition='right'>
@@ -115,15 +115,46 @@ class Home extends Component {
                 </div>
               </div>
             </div>
-            {/* <footer>
-              <div className='social-media'>
-                <a href='https://instagram.com' target='_blank' rel='noopener noreferrer'><Icon type='instagram' style={{ fontSize: '40px', marginRight: '40px' }} /></a>
-                <a href='https://facebook.com' target='_blank' rel='noopener noreferrer'><Icon type='facebook' style={{ fontSize: '40px' }}/></a>
+            <div id='about' className='pd-about'>
+              <div className='page-title'>About Us</div>
+              <div className='content-wrapper'>
+                <span>
+                  Cillum in veniam non excepteur anim excepteur cillum. Dolore sit et irure ullamco esse dolore proident duis veniam id. Anim occaecat pariatur sit irure excepteur eiusmod labore. 
+                </span>
+                <h1>Our Promise</h1>
+                <span>
+                  Non proident sunt esse laborum. Reprehenderit dolore laboris pariatur laborum ea minim nisi minim labore nisi. 
+                </span>
+                <div className='team-wrapper'>
+                  <div>
+                    <div className='team-photo'/>
+                    <div className='team-photo'/>
+                    <div className='team-introduction'>
+                      <div className='team-introduction-title'>
+                        Meet The Team
+                      </div>
+                      <div className='team-introduction-subtitle'>
+                        Designer, Thinker & Collabrators
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className='team-photo'/>
+                    <div className='team-photo'/>
+                    <div className='team-photo'/>
+                  </div>
+                </div>
               </div>
-              <a href='https://squarespace.com' target='_blank' rel='noopener noreferrer' className='blog'>
-                Blog
-              </a>
-            </footer> */}
+              <div className='pd-about-footer'>
+                <div className='title'>
+                  Let's make something great together
+                </div>
+                <div className='subtitle'>
+                  Get in touch with us and send some basic info for a quick quote
+                </div>
+                <Button>Start A Project</Button>
+              </div>
+            </div>
           </div>
         </div>
         <div className='pd-homepage-footer'>
