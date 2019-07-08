00a9fc2c15913f65a8ee2dc4a6383cd11722b7fc
Billy Tjiptoning <8053504+bernardusbilly@users.noreply.github.com>
2019-07-08 13:33:09 +0700

    Updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
-echo "Creating new build gh-pages branch locally"
-git stash
-npm run build
-git add .
-git commit -m "Creating production build"
-
-# Todo: Ask if user wants to push their current build to their current working branch
+# Stashing any working changes
+# Todo: Check if there is any changes from git status
+git stash --include-untracked
 
 echo "Deleting gh-pages branch locally"
 # Todo: Check if gh-pages exists locally
 git branch -D gh-pages
 
-echo "Take the specified branch to 'gh-pages' branch"
+# Saving the working current branch to go back to it later
 BRANCH_NAME=$(git branch | grep \* | cut -d ' ' -f2)
-git checkout ${BRANCH_NAME}
-git checkout gh-pages
+
+# Take the specified branch to 'gh-pages' branch
+git checkout -b gh-pages
+
+echo "Creating a production build and commit the changes"
+npm run build
+git add .
+git commit -m "Creating production build"
+
 git push -f origin gh-pages
+
+# Bring user back to the the previously working branch
+git checkout ${BRANCH_NAME}
+git stash pop
