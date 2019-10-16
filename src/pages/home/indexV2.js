cbf64b737cbbe362551731f4d3a4f02aab19791b
mlutfiibra <mlutfiibra@outlook.com>
2019-10-16 17:48:13 +0700

    Implement ant carousel

--- src/pages/home/indexV2.js
+++ src/pages/home/indexV2.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Tabs } from 'antd';
 
 import Container from 'components/containers/v2';
 
 import AboutUs from 'sections/about_us/v2';
 import Contact from 'sections/contact/v2';
 import Header from 'components/headers/v2';
+import Headline from 'sections/headline/v2';
 import Footer from 'components/footers/v2';
 import MeetTheTeam from 'sections/meet_the_team/v2';
 import OurServices from 'sections/our_services/v2';
@@ -20,37 +20,9 @@ function Home() {
     <div className="pd-homepage">
       <Container>
         <Header />
+        <Headline />
         <div className="pd-homepage-main">
           <div id="home" className="pd-content">
-            <Tabs tabPosition="right">
-              <Tabs.TabPane tab="•" key="1" className="tab-1">
-                <div>We</div>
-                <div>Build</div>
-                <div>Identity.</div>
-                <p>
-                  PandaTech is a full service Branding, design, and development
-                  studio based in the heart of Jakarta.
-                </p>
-              </Tabs.TabPane>
-              <Tabs.TabPane tab="•" key="2" className="tab-2">
-                <div>Create</div>
-                <div>Magnificant</div>
-                <div>Application</div>
-                <p>
-                  From website, mobile app, to desktop app based on your need.
-                </p>
-              </Tabs.TabPane>
-              <Tabs.TabPane tab="•" key="3" className="tab-3">
-                <div>Also</div>
-                <div>Tech</div>
-                <div>Consulting</div>
-                <p>
-                  We also provide the service for consulting your business from
-                  the tech side.
-                </p>
-              </Tabs.TabPane>
-            </Tabs>
-
             <OurServices />
             <PastWorks />
             <StartProject />
---
+++ src/sections/headline/v2.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Carousel } from 'antd';
+
+import './style.scss';
+
+export default function Headline() {
+  return (
+    <section className="pd-headline__v2">
+      <Carousel autoplay>
+        <div className="item">
+          <div className="primary">We</div>
+          <div className="secondary">Build</div>
+          <div className="primary">Identity.</div>
+          <p className="description">
+            PandaTech is a full service Branding, design, and development studio
+            based in the heart of Jakarta.
+          </p>
+        </div>
+        <div className="item">
+          <div className="primary">Create</div>
+          <div className="secondary">Magnificant</div>
+          <div className="primary">Application</div>
+          <p className="description">
+            From website, mobile app, to desktop app based on your need.
+          </p>
+        </div>
+        <div className="item">
+          <div className="primary">Also</div>
+          <div className="secondary">Tech</div>
+          <div className="primary">Consulting</div>
+          <p className="description">
+            We also provide the service for consulting your business from the
+            tech side.
+          </p>
+        </div>
+      </Carousel>
+    </section>
+  );
+}
