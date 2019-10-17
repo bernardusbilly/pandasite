22b28a67083ae3de161185e818b08b9fb8587b51
mlutfiibra <mlutfiibra@outlook.com>
2019-10-17 10:51:05 +0700

    Implementing about-us background

--- src/sections/about_us/v2.js
+++ src/sections/about_us/v2.js
@@ -6,36 +6,40 @@ import './style.scss';
 export default function AboutUs() {
   return (
     <section id="about" className="pd-about">
-      <Row>
-        <Col xs={24} lg={24} className="page-title">
-          About Us
-        </Col>
+      <div className="background">
+        <div className="content-wrapper">
+          <Row>
+            <Col xs={24} lg={24} className="page-title">
+              About Us
+            </Col>
 
-        <div className="about-description">
-          <p>
-            We are a team of collaborators, thinkers, designer and creatives who
-            share the same passion in product development and technologies. With
-            more than 30 years of combined experience in the tech industry, we
-            understand your pain points and strive to bring your business to the
-            next level.
-          </p>
-          <p>
-            Our team are UC Berkeley, Stanford and Purdue graduates who have
-            worked in Silicon Valley, Singapore and China. We take great pride
-            in our work and ensure the best result is delivered.
-          </p>
-        </div>
-      </Row>
+            <div className="about-description">
+              <p>
+                We are a team of collaborators, thinkers, designer and creatives
+                who share the same passion in product development and
+                technologies. With more than 30 years of combined experience in
+                the tech industry, we understand your pain points and strive to
+                bring your business to the next level.
+              </p>
+              <p>
+                Our team are UC Berkeley, Stanford and Purdue graduates who have
+                worked in Silicon Valley, Singapore and China. We take great
+                pride in our work and ensure the best result is delivered.
+              </p>
+            </div>
+          </Row>
 
-      <Row>
-        <Col xs={24} lg={24} className="page-subtitle">
-          Our Promise
-        </Col>
-        <div className="about-description">
-          We only deliver high quality work. Your satisfaction is at the core of
-          our work.
+          <Row>
+            <Col xs={24} lg={24} className="page-subtitle">
+              Our Promise
+            </Col>
+            <div className="about-description">
+              We only deliver high quality work. Your satisfaction is at the
+              core of our work.
+            </div>
+          </Row>
         </div>
-      </Row>
+      </div>
     </section>
   );
 }
