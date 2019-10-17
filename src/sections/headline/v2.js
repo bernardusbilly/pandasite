7ff760c299571457c1e7a5a0eb633cb177cec6be
mlutfiibra <mlutfiibra@outlook.com>
2019-10-17 15:30:42 +0700

    Responsive headline

--- src/sections/headline/v2.js
+++ src/sections/headline/v2.js
@@ -8,26 +8,32 @@ export default function Headline() {
     <section className="pd-headline__v2">
       <Carousel autoplay>
         <div className="item">
-          <div className="primary">Your</div>
-          <div className="secondary">Tech</div>
-          <div className="primary">Professional</div>
+          <div className="title">
+            <div className="primary">Your</div>
+            <div className="secondary">Tech</div>
+            <div className="primary">Professional</div>
+          </div>
           <p className="description">
             PandaTech is a full service Branding, design, and development studio
             based in the heart of Jakarta.
           </p>
         </div>
         <div className="item">
-          <div className="primary">Create</div>
-          <div className="secondary">Magnificant</div>
-          <div className="primary">Application</div>
+          <div className="title">
+            <div className="primary">Create</div>
+            <div className="secondary">Magnificant</div>
+            <div className="primary">Application</div>
+          </div>
           <p className="description">
             From website, mobile app, to desktop app based on your need.
           </p>
         </div>
         <div className="item">
-          <div className="primary">Also</div>
-          <div className="secondary">Tech</div>
-          <div className="primary">Consulting</div>
+          <div className="title">
+            <div className="primary">Also</div>
+            <div className="secondary">Tech</div>
+            <div className="primary">Consulting</div>
+          </div>
           <p className="description">
             We also provide the service for consulting your business from the
             tech side.
